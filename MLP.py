41d2ac6648afe7b3662c9f48fedf27a32ade22e6
Daniel <daniel.fuentesl@e-campus.uab.cat>
2020-03-01 11:24:50 +0100

    82% accuracy

--- MLP.py
+++ MLP.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import torch
 import torch.nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import transforms
 import torchvision
@@ -22,10 +23,10 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 input_size = 784
 hidden_size = 500
 num_classes = 8
-num_epochs = 1
-batch_size = 1
-learning_rate = 0.001
-momentum = 0.9
+num_epochs = 150
+batch_size = 32
+learning_rate = 0.01
+momentum = 0.0
 
 # MNIST dataset
 #train_dataset = torchvision.datasets.MNIST(root='../../data',
@@ -34,7 +35,7 @@ momentum = 0.9
 #                                           download=True)
 
 #train_dataset = DatasetW(True)
-test_dataset = DatasetW(False)
+#test_dataset = DatasetW(False)
 #test_dataset = torchvision.datasets.MNIST(root='../../data',
 #                                          train=False,
 #                                          transform=transforms.ToTensor())
@@ -45,24 +46,27 @@ test_dataset = DatasetW(False)
 #                                           batch_size=batch_size,
 #                                           shuffle=True)
 TRAIN_DATA_PATH = "datasets/MIT_split/train"
-TEST_DATA_PATH = "./images/test/"
+TEST_DATA_PATH = "datasets/MIT_split/test"
+
 TRANSFORM_IMG = transforms.Compose([
     #transforms.Resize(256),
     #transforms.CenterCrop(256),
-    transforms.ToTensor(),
+    transforms.ToTensor()
     #transforms.Normalize(mean=[0.485, 0.456, 0.406],
     #                     std=[0.229, 0.224, 0.225] )
     ])
 
 train_data = torchvision.datasets.ImageFolder(root=TRAIN_DATA_PATH, transform=TRANSFORM_IMG)
+test_data = torchvision.datasets.ImageFolder(root=TEST_DATA_PATH, transform=TRANSFORM_IMG)
 trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
 #DATA_PATH = '\PycharmProjects\MNISTData'
 #train_data =torchvision.datasets.MNIST(root=DATA_PATH, train=True, transform=trans, download=True)
 train_loader = torch.utils.data.DataLoader(dataset=train_data,
                                           batch_size=batch_size,
                                           shuffle=False,
-                                          drop_last=True)
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          drop_last=True
+                                           )
+test_loader = torch.utils.data.DataLoader(dataset=test_data,
                                           batch_size=batch_size,
                                           shuffle=False,
                                           drop_last=True)
@@ -80,8 +84,8 @@ def imshow(img):
 
 
 # get some random training images
-dataiter = iter(train_loader)
-images, labels = dataiter.next()
+#dataiter = iter(train_loader)
+#images, labels = dataiter.next()
 
 # show images
 #imshow(torchvision.utils.make_grid(images))
@@ -115,27 +119,8 @@ class MLP(torch.nn.Module):
         self.fc1 = torch.nn.Linear(23 * 23 * 256, 128)
         self.batch = torch.nn.BatchNorm1d(128)
         self.fc2 = torch.nn.Linear(128, 8)
-        """
-        self.fc1 = torch.nn.Conv2d(3, 32, kernel_size=(3,3))
-        self.fc2 = torch.nn.BatchNorm2d(32)
-        self.fc3 = torch.nn.MaxPool2d(2)
-
-        self.fc4 = torch.nn.Conv2d(32, 64, kernel_size=(3,3))
-        self.fc5 = torch.nn.BatchNorm2d(64)
-        self.fc6 = torch.nn.MaxPool2d(2)
-
-        self.fc7 = torch.nn.Conv2d(64, 256, kernel_size=(1,1))
-        self.fc8 = torch.nn.BatchNorm2d(256)
-
-        self.fc9 = torch.nn.AvgPool2d(kernel_size=(3,3))
-
-        self.fc10 = torch.nn.Linear(102400, 128)
-        self.fc11 = torch.nn.ReLU()
-        self.fc12 = torch.nn.Softmax(dim=1)
-        self.out = torch.nn.Linear(128, 8)
-        self.fc13 = torch.nn.BatchNorm2d(128)
-        self.fc14 = torch.nn.Dropout(0.2)
-        """
+        self.act = torch.nn.ReLU()
+        self.soft = torch.nn.Softmax()
 
     def forward(self, x):
         """Build an actor (policy) network that maps states -> actions."""
@@ -149,54 +134,32 @@ class MLP(torch.nn.Module):
 
         out = self.fc1(out)
         #print(out.shape)
+        #out = self.drop_out(out)
+        #out = self.soft(out)
+        #out = self.fc2(out)
+        out = self.act(out)  # [batch_size, 128]
         out = self.drop_out(out)
-        #print(out.shape)
-#        out = self.batch(out)
         out = self.fc2(out)
-        #print(out.shape)
+        #out = self.soft(out)
+        print(out.shape)
+
         return out
 
-
-
-"""
-        x = F.relu(self.fc1(state))
-        x = self.fc2(x)
-        x = self.fc3(x)
-        print(x.shape)
-        x = self.fc4(x)
-        x = self.fc5(x)
-        x = self.fc6(x)
-        print(x.shape)
-        x = self.fc7(x)
-        x = self.fc8(x)
-        x = self.fc9(x)
-        print(x.shape)
-         # [batch_size, 28*13*13=4732]
-        #x = x.view(-1, 20)
-        x = F.relu(self.fc10(x))
-        print(x.shape)
-        x = self.fc14(x)
-        print(x.shape)
-        x = self.fc13(x)
-        print(x.shape)
-        x = self.out(x)
-        print(x.shape)
-        x = self.fc12(x)
-        print(x.shape)
-        return x
-"""
 model = MLP().to(device)
 #print(model)
 # Loss and optimizer
 criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum = momentum)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum = momentum, nesterov=False)
 
 # Train the model
 total_step = len(train_loader)
 for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader, 0):
+    for i, (images, labels) in enumerate(train_loader):
+
         # Move tensors to the configured device
         #images = images.reshape(-1, 28*28).to(device)
+
+
         images = images.to(device)
         labels = labels.to(device)
 
@@ -211,9 +174,12 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
-        if (i+1) % 100 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+        # Accuracy
+        outputs = (outputs > 0.5).float()
+        correct = (outputs == labels[:8]).float().sum()
+
+        #if (i+1) % 100 == 0:
+        print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Acc: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item(), correct/outputs.shape[0]))
 
 # Test the model
 # In test phase, we don't need to compute gradients (for memory efficiency)
@@ -221,7 +187,9 @@ with torch.no_grad():
     correct = 0
     total = 0
     for images, labels in test_loader:
-        images = images.reshape(-1, 28*28).to(device)
+
+        #images = images.reshape(-1, 28*28).to(device)
+        images = images.to(device)
         labels = labels.to(device)
         outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
---
+++ tgpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -n 4 # Number of cores
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH -D . # working directory
+#SBATCH -t 0-00:05 # Runtime in D-HH:MM
+#SBATCH -p dcc # Partition to submit to
+#SBATCH --mem 2048 # 2GB solicitados.
+#SBATCH --gres gpu:Pascal:1 # Para pedir Pascales MAX 8
+#SBATCH -o %x_%u_%j.out # File to which STDOUT will be written
+#SBATCH -e %x_%u_%j.err # File to which STDERR will be written
+python3 MLP.py
