b0a3bf94be0ceb3f55dffa4361af59fd20e92162
Marc Orós <marcoc619@gmail.com>
2020-03-14 22:48:58 +0100

    Separate cluster scripts and finished remaining dataset-model combos

--- Week3/TaskD/F-CNN_KITTI.py
+++ Week3/TaskD/F-CNN_KITTI.py
@@ -15,21 +15,24 @@ from detectron2.utils.logger import setup_logger
 from KITTIMOTSLoader import get_KITTIMOTS_dicts
 
 for d in ["train", "val"]:
-    DatasetCatalog.register("kittimots" + d, lambda d=d: get_KITTIMOTS_dicts(d))
-    MetadataCatalog.get("kittimots" + d).set(thing_classes=['Car', 'Pedestrian', 'DontCare'])
-kitti_metadata = MetadataCatalog.get("kittimotstrain")
+    DatasetCatalog.register("fcnn-kittimots" + d, lambda d=d: get_KITTIMOTS_dicts(d))
+    MetadataCatalog.get("fcnn-kittimots" + d).set(thing_classes=['Car', 'Pedestrian', 'DontCare'])
+kitti_metadata = MetadataCatalog.get("fcnn-kittimotstrain")
 
 dataset_dicts = get_KITTIMOTS_dicts('train')
 
 cfg = get_cfg()
 cfg.merge_from_file("./detectron2_repo/configs/COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")
-cfg.DATASETS.TRAIN = ("kittimotstrain",)
+cfg.DATASETS.TRAIN = ("fcnn-kittimotstrain",)
 cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
 cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = "detectron2://COCO-Detection/faster_rcnn_R_101_FPN_3x/137851257/model_final_f6e8b1.pkl"
 cfg.SOLVER.IMS_PER_BATCH = 4
 cfg.SOLVER.BASE_LR = 0.00025
-cfg.SOLVER.MAX_ITER = 1 * len(dataset_dicts) // cfg.SOLVER.IMS_PER_BATCH + 1
+itersToFullDataset = len(dataset_dicts) // cfg.SOLVER.IMS_PER_BATCH + 1
+itersMultiplier = 5
+itersDivider = 1
+cfg.SOLVER.MAX_ITER = itersMultiplier * itersToFullDataset // itersDivider
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3
 
@@ -43,8 +46,8 @@ cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set the testing threshold for this model
 
 # Set training data-set path
-cfg.DATASETS.TEST = ("kittimotsval", )
+cfg.DATASETS.TEST = ("fcnn-kittimotsval", )
 
 
-evaluator = COCOEvaluator("kittimotsval", cfg, False, output_dir="./output/")
+evaluator = COCOEvaluator("fcnn-kittimotsval", cfg, False, output_dir="./output/")
 trainer.test(cfg, trainer.model, evaluators=[evaluator])
--- Week3/TaskD/F-CNN_MOTS.py
+++ Week3/TaskD/F-CNN_MOTS.py
@@ -11,97 +11,30 @@ from detectron2.data import MetadataCatalog
 from detectron2.engine import DefaultPredictor, DefaultTrainer
 from detectron2.utils.visualizer import Visualizer
 from detectron2.utils.logger import setup_logger
-setup_logger('zutput_file')
 
-PATH_TRAIN = '/home/mcv/datasets/KITTI/data_object_image_2/mini_train'
-PATH_TEST = '/home/mcv/datasets/KITTI/data_object_image_2/testing/image_2'
-
-
-def get_KITTI_dicts(set_type):
-    categories = {
-        'Car': 0,
-        'Van': 1,
-        'Truck': 2,
-        'Pedestrian': 3,
-        'Person_sitting': 4,
-        'Cyclist': 5,
-        'Tram': 6,
-        'Misc': 7,
-        'DontCare': 8
-    }
-
-    if set_type is 'mini_train':
-        working_folder = '/home/mcv/datasets/KITTI/data_object_image_2/mini_train/'
-    if set_type is 'training':
-        working_folder = '/home/mcv/datasets/KITTI/data_object_image_2/training/image_2'
-    if set_type is 'testing':
-        working_folder = '/home/mcv/datasets/KITTI/data_object_image_2/testing/image_2'
-
-    # obtain
-    # image_dir='/home/mcv/datasets/KITTI/data_object_image_2/training/image_2'
-    # image_dir='/Users/danielfuentes/Desktop/KITTI/data_object_image_2/mini_train'
-    # label_dir='/Users/danielfuentes/Desktop/KITTI/training/label_2'
-    label_dir = '/home/mcv/datasets/KITTI/training/label_2'
-    image_path = glob.glob(working_folder + '/*.png')
-    label_path = glob.glob(label_dir + '/*.txt')
-    label_file = sorted(label_path)
-    image_file = sorted(image_path)
-    for file in image_file:
-        splitd = file.split(os.sep)
-        img_name = splitd[-1]
-        img_id = img_name.split('.')[0]
-        if set_type is not 'testing':
-            label_file.append(label_dir + img_id + '.txt')
-
-    dataset_dicts = []
-
-    # iteration
-    for i in range(0, len(image_file)):
-        record = {}
-        height, width = cv2.imread(image_file[i]).shape[:2]
-        record["file_name"] = image_file[i]
-        record["image_id"] = i
-        record["height"] = height
-        record["width"] = width
-
-        if set_type is not 'testing':
-            objs = []
-            with open(label_file[i]) as f:
-                lines = f.readlines()
-            if len(lines) is 0:
-                print('No lines in file')
-                exit()
-            for line in lines:
-                col = line.split()
-                catg = categories[col[0]]
-                obj = {
-                    "bbox": [float(col[4]), float(col[5]), float(col[6]), float(col[7])],
-                    "bbox_mode": BoxMode.XYXY_ABS,
-                    "category_id": catg
-                }
-                objs.append(obj)
-            record["annotations"] = objs
-        dataset_dicts.append(record)
-    return dataset_dicts
+from MOTSChallengeLoader import get_MOTS_dicts
 
 for d in ["train", "val"]:
-    DatasetCatalog.register("kitti" + d, lambda d=d: get_KITTI_dicts('training' if d == 'train' else 'testing'))
-    MetadataCatalog.get("kitti" + d).set(thing_classes=['Car', 'Van', 'Truck', 'Pedestrian', 'Person_sitting', 'Cyclist', 'Tram', 'Misc', 'DontCare'])
-kitti_metadata = MetadataCatalog.get("kittitrain")
+    DatasetCatalog.register("fcnn-mots" + d, lambda d=d: get_MOTS_dicts(d))
+    MetadataCatalog.get("fcnn-mots" + d).set(thing_classes=['Car', 'Pedestrian', 'DontCare'])
+kitti_metadata = MetadataCatalog.get("fcnn-motstrain")
 
-dataset_dicts = get_KITTI_dicts('testing')
+dataset_dicts = get_MOTS_dicts('train')
 
 cfg = get_cfg()
 cfg.merge_from_file("./detectron2_repo/configs/COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")
-cfg.DATASETS.TRAIN = ("kittitrain",)
+cfg.DATASETS.TRAIN = ("fcnn-motstrain",)
 cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
 cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = "detectron2://COCO-Detection/faster_rcnn_R_101_FPN_3x/137851257/model_final_f6e8b1.pkl"
 cfg.SOLVER.IMS_PER_BATCH = 4
 cfg.SOLVER.BASE_LR = 0.00025
-cfg.SOLVER.MAX_ITER = 5 * len(dataset_dicts) // cfg.SOLVER.IMS_PER_BATCH + 1 
+itersToFullDataset = len(dataset_dicts) // cfg.SOLVER.IMS_PER_BATCH + 1
+itersMultiplier = 5
+itersDivider = 1
+cfg.SOLVER.MAX_ITER = itersMultiplier * itersToFullDataset // itersDivider
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 9
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 trainer = DefaultTrainer(cfg)
@@ -113,7 +46,8 @@ cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set the testing threshold for this model
 
 # Set training data-set path
-cfg.DATASETS.TEST = ("kittitrain", )
+cfg.DATASETS.TEST = ("fcnn-motsval", )
 
-evaluator = COCOEvaluator("kittitrain", cfg, False, output_dir="./output/")
+
+evaluator = COCOEvaluator("fcnn-motsval", cfg, False, output_dir="./output/")
 trainer.test(cfg, trainer.model, evaluators=[evaluator])
