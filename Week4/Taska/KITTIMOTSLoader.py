89aa81cda734b396a62e8268cfeae07827a2134b
Daniel <daniel.fuentesl@e-campus.uab.cat>
2020-03-21 22:26:07 +0100

    Completed task a

--- Week4/Taska/KITTIMOTSLoader.py
+++ Week4/Taska/KITTIMOTSLoader.py
@@ -9,7 +9,8 @@ from detectron2.structures import BoxMode
 import mask
 import pickle
 
-
+coco_to_kitti_dict = {1: 2, 2: 0 }
+KITTI_CATEGORIES = [0, 1, 2]
 def get_KITTIMOTS_dicts(set_type):
     if set_type is not 'train' and set_type is not 'test' and set_type is not 'val':
         raise Exception("Invalid set type")
@@ -48,14 +49,17 @@ def get_KITTIMOTS_dicts(set_type):
 
         for line in lines:
             col = line.split()
+            #if int(col[2]) not in KITTI_CATEGORIES:
+            #    continue
             if imageNum is int(col[0]):
                 catg = int(col[1]) // 1000
+
                 if catg is 1:
-                    catg = 0
-                elif catg is 2:
-                    catg = 1
-                else:
                     catg = 2
+                elif catg is 2:
+                    catg = 0
+                else:
+                    catg = 1
                 rle = {
                     'counts': col[5].strip(),
                     'size': [height, width]
