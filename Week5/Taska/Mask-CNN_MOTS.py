e86692bdeb38fd968a3a14d1beee5b8761bc1a44
Daniel <daniel.fuentesl@e-campus.uab.cat>
2020-03-27 18:24:16 +0100

    week 5 task a

---
+++ Week5/Taska/Mask-CNN_MOTS.py
@@ -0,0 +1,100 @@
+import os
+import sys
+import glob
+import cv2
+
+from detectron2.config import get_cfg
+from detectron2.data import DatasetCatalog
+from detectron2.data import MetadataCatalog
+from detectron2.engine import DefaultPredictor
+from detectron2.utils.visualizer import Visualizer, ColorMode
+from detectron2.utils.logger import setup_logger
+from detectron2.modeling import build_model
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.data import build_detection_test_loader
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+setup_logger()
+
+file_dir = os.path.dirname(__file__)
+print(file_dir)
+sys.path.append(file_dir)
+
+from KITTIMOTSLoader import get_KITTIMOTS_dicts
+import MaskConfiguration as mk
+
+inference = False
+class MaskCNN_MOTS(object):
+    def run(self, argv):
+
+        if len(argv) == 1:
+            exit(0)
+        else:
+            conf = argv[1]
+            configuration = mk.MaskConfiguration().get_Configuration(conf)
+
+        PATH_RESULTS = './Results-{}/'.format(conf)
+        PATH_TRAIN = '/home/mcv/datasets/KITTI-MOTS/training/image_02/'
+        os.makedirs(PATH_RESULTS, exist_ok=True)
+
+        print("////////////////////////////////////////////////////////")
+        print("////////////////////Configuration///////////////////////")
+        print("////////////////////////////////////////////////////////")
+        print(configuration[0])
+        print(configuration[1])
+        print("////////////////////////////////////////////////////////")
+        print("////////////////////////////////////////////////////////")
+        print("////////////////////////////////////////////////////////")
+
+        for d in ["train", "val"]:
+            DatasetCatalog.register("fcnn-mots" + d, lambda d=d: get_KITTIMOTS_dicts(d))
+            MetadataCatalog.get("fcnn-mots" + d).set(thing_classes=['Car', 'DontCare','Pedestrian'])
+
+        # Inference
+        cfg = get_cfg()
+        cfg.merge_from_file(configuration[0])
+        metasata = MetadataCatalog.get(cfg.DATASETS.TRAIN[0])
+        cfg.DATASETS.TRAIN = ('fcnn-motstrain',)
+        cfg.DATASETS.TEST = ('fcnn-motsval',)
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+        cfg.OUTPUT_DIR = PATH_RESULTS
+        cfg.MODEL.WEIGHTS = configuration[1]
+
+        if(inference):
+            predictor = DefaultPredictor(cfg)
+            for filePath in glob.glob(PATH_TRAIN + '/*/*.png'):
+                path, filename = os.path.split(filePath)
+                print(filePath)
+                # Make prediction
+                im = cv2.imread(filePath)
+                outputs = predictor(im)
+                v = Visualizer(
+                    im[:, :, ::-1],
+                    metadata=metasata,
+                    scale=0.8,
+                    instance_mode=ColorMode.IMAGE)
+                v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+
+                os.makedirs(PATH_RESULTS, exist_ok=True)
+                cv2.imwrite(PATH_RESULTS + filename, v.get_image()[:, :, ::-1])
+
+        print("Generating Predictions")
+        model = build_model(cfg)
+        DetectionCheckpointer(model).load(cfg.MODEL.WEIGHTS)
+
+        # Evaluation
+        coco_to_kitti_dict = {2: 0, 0: 1,}
+        evaluator = COCOEvaluator('fcnn-motsval', cfg, False, output_dir='./output{}'.format(conf))
+        val_loader = build_detection_test_loader(cfg, 'fcnn-motsval')
+        inference_on_dataset(model, val_loader, evaluator)
+        preds = evaluator._predictions
+        for pred in preds:
+            pred['instances'] = [i for i in pred['instances'] if i['category_id'] in coco_to_kitti_dict.keys()]
+            for instance in pred['instances']:
+                instance['category_id'] = coco_to_kitti_dict[instance['category_id']]
+        evaluator._predictions = preds
+        evaluator.evaluate()
+
+if __name__ == '__main__':
+    MaskCNN_MOTS().run(sys.argv)
+
+
---
+++ Week5/Taska/MaskConfiguration.py
@@ -0,0 +1,27 @@
+class MaskConfiguration:
+    def __init__(self):
+        self.configurations = {
+        "R50-C4" : ('/home/grupo09/df/Week2/detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_C4_1x.yaml' ,
+                          'detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_C4_1x/137259246/model_final_9243eb.pkl'),
+
+        "R50-DC5" : ('/home/grupo09/df/Week2/detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_DC5_1x.yaml',
+                          'detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_DC5_1x/137260150/model_final_4f86c3.pkl'),
+
+        "R50-FPN" : ('/home/grupo09/df/Week2/detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml',
+                          'detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x/137260431/model_final_a54504.pkl'),
+
+        "R101-C4" : ('/home/grupo09/df/Week2/detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_101_C4_3x.yaml',
+                          'detectron2://COCO-InstanceSegmentation/mask_rcnn_R_101_C4_3x/138363239/model_final_a2914c.pkl'),
+
+        "R101-DC5" : ('/home/grupo09/df/Week2/detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_101_DC5_3x.yaml',
+                          'detectron2://COCO-InstanceSegmentation/mask_rcnn_R_101_DC5_3x/138363294/model_final_0464b7.pkl'),
+
+        "R101-FPN" : ('/home/grupo09/df/Week2/detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml',
+                          'detectron2://COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x/138205316/model_final_a3ec72.pkl'),
+
+        "R50-FPN-CS" : ('/home/grupo09/df/Week2/detectron2_repo/configs/Cityscapes/mask_rcnn_R_50_FPN.yaml',
+                          'detectron2://Cityscapes/mask_rcnn_R_50_FPN/142423278/model_final_af9cf5.pkl')
+        }
+
+    def get_Configuration(self, configuration):
+        return self.configurations.get(configuration)
